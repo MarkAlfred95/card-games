af41bb0eb6be90a671e36d9f5a6fd5b4be761256
“MarkAlfred95” <markalfredtornado@gmail.com>
2026-06-30 15:43:24 +0800

    feat: add constants and index files for Pusoy Trese game rules and components

---
+++ src/components/game/pusoy-trese/index.ts
@@ -0,0 +1,9 @@
+// Public surface for the Pusoy Trese feature components.
+export { default as Header } from "./Header";
+export { default as PokerTable } from "./PokerTable";
+export { default as BettingGate } from "./BettingGate";
+export { default as HandZone } from "./HandZone";
+export { default as Seat } from "./Seat";
+export { default as Picker } from "./Picker";
+export { default as SettingsMenu } from "./SettingsMenu";
+export * from "./constants";
